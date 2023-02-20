39b6cabf9a531b9b458d78e33cfbeb3188bea5cc
dunky-star <dukygeoffrey39@gmail.com>
2023-02-20 20:10:35 +0300

    Complete coding quiz component ts file

--- online-tests-app/src/app/components/quiz/quiz.component.ts
+++ online-tests-app/src/app/components/quiz/quiz.component.ts
@@ -19,87 +19,87 @@ export class QuizComponent implements OnInit {
   interval$: any;
   progress: string = '0';
   isQuizCompleted: boolean = false;
-  // Injecting Service
-  constructor(private questionService: QuizService) {}
+  // Injecting Service to the Quiz component.
+  constructor(private quizService: QuizService) {}
 
-  // before render component it init's (automatically)
+  // Intialization before rendering
   ngOnInit(): void {
     this.name = localStorage.getItem('name')!;
     this.getAllQuestions();
     this.startCounter();
   }
-  // fetching questions from JSON / API and adding inside array
+  // Fetching quiz from the API and adding to array
   getAllQuestions() {
-    this.questionService
+    this.quizService
       .getQuestionJson()
       .subscribe((res: { questions: any }) => {
         this.questionList = res.questions;
       });
   }
-  // switching to next question
+  // Next question
   nextQuestion() {
     this.currentQuestion++;
   }
-  // switch back to previous question (opt)
+  // Previous question
   previousQuestion() {
     this.currentQuestion--;
   }
 
-  // answer fun() - with parameter of question & their options
+  // answer with parameter of question and their options.
   answer(currentQno: number, option: any) {
     if (currentQno === this.questionList.length) {
       this.isQuizCompleted = true;
       this.stopCounter();
     }
-    // if the ans is correct !
+    // if the answer is correct
     if (option.correct) {
-      this.points += 10; // increase 10 points
-      this.correctAnswer++; // correct ans count + 1
+      this.points += 5; // increase by 5 points
+      this.correctAnswer++; // correct answer count + 1
       setTimeout(() => {
-        this.currentQuestion++; // increase qst count + 1
+        this.currentQuestion++; // increase question count + 1
         this.resetCounter(); // reset timer
-        this.getProgressPercent(); // increase progressbar percentage
+        this.getProgressPercent(); // increase progressbar %
       }, 1000);
     }
-    // if we selected wrong ans !
+    // if wrong answer selected
     else {
       setTimeout(() => {
-        this.currentQuestion++; // move next qst
+        this.currentQuestion++; // move to next question
         this.inCorrectAnswer++; // incorrect count + 1
         this.resetCounter(); // reset time
-        this.getProgressPercent(); // increase progressbar percentage
+        this.getProgressPercent(); // increase progressbar %
       }, 1000);
 
-      this.points -= 10; // minus points
+      this.points -= 5; // substract 5 points
     }
   }
-  // Starting counter
+  // Start counter
   startCounter() {
-    this.interval$ = interval(1000) // interval 1sec
+    this.interval$ = interval(1000) // interval is 1 sec
       .subscribe((val) => {
-        this.counter--; // decrease 60sec to 0sec
+        this.counter--; // decrease 60 sec to 0sec
         if (this.counter === 0) {
-          this.currentQuestion++; //increase qst count + 1
-          this.counter = 10; // timer for 60sec
-          this.points -= 10; // if not answered within 60sec it will take minus points
+          this.currentQuestion++; //increase question count + 1
+          this.counter = 5; // timer for 60 sec
+          this.points -= 5; // if not answered within 60 sec it will take minus points
         }
       });
     setTimeout(() => {
       this.interval$.unsubscribe(); // idle time limit for 60sec
     }, 100000);
   }
-  // stop counter fun()
+  // stop counter
   stopCounter() {
     this.interval$.unsubscribe();
     this.counter = 0;
   }
-  // reset counter fun()
+  // reset counter
   resetCounter() {
     this.stopCounter();
     this.counter = 10;
     this.startCounter();
   }
-  // reset quiz - starts from begining
+  // reset question - starts from beginning again
   resetQuiz() {
     this.resetCounter();
     this.getAllQuestions();
@@ -108,7 +108,7 @@ export class QuizComponent implements OnInit {
     this.currentQuestion = 0;
     this.progress = '0';
   }
-  // progressbar fun()
+  // progressbar status
   getProgressPercent() {
     this.progress = (
       (this.currentQuestion / this.questionList.length) *
